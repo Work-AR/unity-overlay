0f1c24563cc09eae35ec1d117ac6a1333f0c6223
thecoder-001 <ayush06feb@gmail.com>
2024-03-13 06:02:31 +0530

    proof of concept

---
+++ Assets/Scripts/Runtime/Util/LogComponent.cs
@@ -0,0 +1,10 @@
+namespace UnityEngine.XR.ARFoundation.test
+{
+    public class LogComponent : MonoBehaviour
+    {
+        public void Log(string s)
+        {
+            Debug.Log(s);
+        }
+    }
+}
